fd86f68eb1f6cd7e805f48db80d67586e686d8a3
Ruiming Guo <guoruiming@stu.scu.edu.cn>
2023-11-19 21:49:29 +0800

    feat: add, subtract, multiply, divide

--- calc.c
+++ calc.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 int add(int a, int b) {
-    return 0;
+    return a + b;
 }
 
 int subtract(int a, int b) {
-    return 0;
+    return a - b;
 }
 
 int multiply(int a, int b) {
-    return 0;
+    return a * b;
 }
 
 int divide(int a, int b) {
-    return 0;
+    return a / b;
 }
